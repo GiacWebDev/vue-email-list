da74632de3339f267340a12a4014802027a65361
GiacWebDev <giacobechini@gmail.com>
2023-10-06 16:42:09 +0200

    [edit] js structure and axios

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,29 @@
+// 1. testo il link con l'estensione THUNDER CLIENT
+
+
 const { createApp } = Vue;
 
 createApp({
   data(){
     return {
-      
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      listaMail: [],
+      mail: '',
     }
-  }
+  },
+
+  methods: {
+    getApi(){
+
+      axios
+          .get(this.apiUrl)
+          .then ((risposta) => {
+            console.log(risposta.data)
+
+          })
+
+    }
+  },
+
 
 }).mount('app')
